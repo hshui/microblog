a0fa3bc800a5b9207d1c7ec77f716b3635ee4b83
hshui <hshui@sfu.ca>
2019-10-05 20:02:12 -0700

    added basic template and turned index.html into block content

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import webapp
 
 @webapp.route('/')
 @webapp.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Herman'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Vancouver!'
+        },
+        {
+            'author': {'username': 'Donald'},
+            'body': 'Someone made me mad again!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
