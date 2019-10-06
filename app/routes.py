509590c80474b303cb5be753a904c06fb1194732
hshui <hshui@sfu.ca>
2019-10-06 15:11:23 -0700

    implemented basic login form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import webapp
+from app.forms import LoginForm
 
 @webapp.route('/')
 @webapp.route('/index')
@@ -17,3 +18,19 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+# @webapp.route('/login')
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title='Sign In', form=form)
+
+# View that supports GET and POST methods
+@webapp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # processes the form. If POST request is called it will run validators attached to fields and return true if everything passes else false 
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
