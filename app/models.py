15fd6a87d5e472fe9fcec1a3451dc9ebfb684ff8
hshui <hshui@sfu.ca>
2019-10-08 23:43:50 -0700

    added post table and flask shell context

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 # Tells us how to create a schema
@@ -7,8 +8,19 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     # Tells Python how to print object of this class
     def __repr__(self):
         return '<User {}>'.format(self.username) 
-        
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), index=True)
+    # Passing function as default value results in setting the field to the value of calling such function (not the result of calling it)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # feeds id value from 'user' table
+
+    
+    def __repr__(self):
+        return '<Post {}>'.format(self.body) 
--- microblog.py
+++ microblog.py
@@ -1,1 +1,7 @@
-from app import webapp
+from app import webapp, db
+from app.models import User, Post
+
+@webapp.shell_context_processor
+def make_shell_context():
+    # map is used to reference a 'name' for each item imported into shell
+    return {'db': db, 'User': User, 'Post': Post}
