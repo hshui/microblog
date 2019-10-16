1265b4c9a3663bb726b8d12195de3e22e68572bc
hshui <hshui@sfu.ca>
2019-10-15 22:38:15 -0700

    implemented explore and updated index view to show posts

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,26 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import webapp, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from datetime import datetime
 
 
-@webapp.route('/')
-@webapp.route('/index')
+@webapp.route('/', methods=['GET', 'POST'])
+@webapp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Vancouver!'
-        },
-        {
-            'author': {'username': 'Donald'},
-            'body': 'Someone made me mad again!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
-
-# @webapp.route('/login')
-# def login():
-#     form = LoginForm()
-#     return render_template('login.html', title='Sign In', form=form)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        # https://en.wikipedia.org/wiki/Post/Redirect/Get
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form, posts=posts)
 
 # View that supports GET and POST methods
 @webapp.route('/login', methods=['GET', 'POST'])
@@ -138,3 +132,10 @@ def unfollow(username):
     db.session.commit()
     flash('You unfollowed {}.'.format(username))
     return redirect(url_for('user', username=username))
+
+
+@webapp.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
